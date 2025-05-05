b23c1ece3fb59e871c163cad1896a44111fc6b48
bazuaga <bazuaga95@fpuna.edu.py>
2025-05-04 23:04:19 -0300

    Ajuste en marcacion para obtener en vista

--- src/main/java/com/gestionatalento/gestiona_talento/Controllers/MarcacionManualController.java
+++ src/main/java/com/gestionatalento/gestiona_talento/Controllers/MarcacionManualController.java
@@ -13,8 +13,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gestionatalento.gestiona_talento.Dto.MarcacionManualDto;
+import com.gestionatalento.gestiona_talento.Dto.MarcacionVistaDto;
+import com.gestionatalento.gestiona_talento.Dto.MarcacionVistaDtoRequest;
 import com.gestionatalento.gestiona_talento.Entity.MarcacionManual;
+import com.gestionatalento.gestiona_talento.Entity.MarcacionVista;
+import com.gestionatalento.gestiona_talento.Entity.TipoEvento;
 import com.gestionatalento.gestiona_talento.Repository.MarcacionManualRepository;
+import com.gestionatalento.gestiona_talento.Repository.MarcacionVistaRepository;
 import com.gestionatalento.gestiona_talento.Response.GenericResponse;
 import com.gestionatalento.gestiona_talento.ServiceImpl.MarcacionManualServiceImpl;
 
@@ -30,6 +35,9 @@ public class MarcacionManualController {
     MarcacionManualRepository marcacionManualRepository;
     @Autowired
     MarcacionManualServiceImpl marcacionManualServiceImpl;
+
+    @Autowired
+    MarcacionVistaRepository marcacionVistaRepository;
 
     @PostMapping("/crear")
     public GenericResponse crearMarcacionManual(@Valid @RequestBody MarcacionManualDto marcacionManualDto) {
@@ -49,6 +57,31 @@ public class MarcacionManualController {
         GenericResponse genericResponse = new GenericResponse();
         try {
             genericResponse = marcacionManualServiceImpl.actualizarMarcacionManual(marcacionManualDto);
+            return genericResponse;
+        } catch (Exception e) {
+            genericResponse.setCodigoMensaje("500");
+            genericResponse.setMensaje("Ha ocurrido un error interno en el servidor: " + e.getMessage());
+            return genericResponse;
+        }
+    }
+
+    @GetMapping("/obtener")
+    public GenericResponse obtenerMarcaciones(@Valid @RequestBody MarcacionVistaDtoRequest marcacionVistaDtoRequest) {
+        GenericResponse genericResponse = new GenericResponse();
+        try {
+            List<MarcacionVistaDto> marcacionVistaDto = new ArrayList<>();
+            marcacionVistaDto = marcacionVistaRepository.obtenerMarcaciones(marcacionVistaDtoRequest.getNroDocumento(), marcacionVistaDtoRequest.getFecDesde(), marcacionVistaDtoRequest.getFecHasta());
+            
+            if (marcacionVistaDto.size() > 0) {
+                genericResponse.setCodigoMensaje("200");
+                genericResponse.setMensaje("Han sido obtenidas todas las marcaciones");
+                genericResponse.setObjeto(marcacionVistaDto);
+            } else {
+                genericResponse.setCodigoMensaje("404");
+                genericResponse.setMensaje("No se han encontrado datos de marcaciones");
+                genericResponse.setObjeto(null);
+            }
+            genericResponse.setCodigoMensaje("200");
             return genericResponse;
         } catch (Exception e) {
             genericResponse.setCodigoMensaje("500");
