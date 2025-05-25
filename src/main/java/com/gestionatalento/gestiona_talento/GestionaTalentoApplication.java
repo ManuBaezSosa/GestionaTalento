0e24963cbf91f8189e74ba44fb275a6f7a395728
bazuaga <bazuaga95@fpuna.edu.py>
2025-05-25 19:25:31 -0300

    Cambios para deployar en la nube

--- src/main/java/com/gestionatalento/gestiona_talento/GestionaTalentoApplication.java
+++ src/main/java/com/gestionatalento/gestiona_talento/GestionaTalentoApplication.java
@@ -1,11 +1,18 @@
-
 package com.gestionatalento.gestiona_talento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 @SpringBootApplication
-public class GestionaTalentoApplication {
+public class GestionaTalentoApplication extends SpringBootServletInitializer {
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(GestionaTalentoApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestionaTalentoApplication.class, args);
