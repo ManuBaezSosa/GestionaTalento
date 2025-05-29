077569b7118885a25d7bd82c3c1bd6744c894d97
bazuaga <bazuaga95@fpuna.edu.py>
2025-05-29 16:32:22 -0300

    Se agrega endpoint de Inventario de Documentos

--- src/main/java/com/gestionatalento/gestiona_talento/Controllers/InventarioDocumentoController.java
+++ src/main/java/com/gestionatalento/gestiona_talento/Controllers/InventarioDocumentoController.java
@@ -1,5 +1,10 @@
 package com.gestionatalento.gestiona_talento.Controllers;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -8,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gestionatalento.gestiona_talento.Dto.InventarioDocumentoDto;
+import com.gestionatalento.gestiona_talento.Entity.Empleado;
+import com.gestionatalento.gestiona_talento.Entity.InventarioDocumento;
 import com.gestionatalento.gestiona_talento.Repository.InventarioDocumentoRepository;
 import com.gestionatalento.gestiona_talento.Repository.DireccionRepository;
 import com.gestionatalento.gestiona_talento.Response.GenericResponse;
@@ -18,7 +25,7 @@ import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.DeleteMapping;
 
 @RestController
-@RequestMapping("/documentos/inventario")
+@RequestMapping("/personas/documentos/inventarios")
 public class InventarioDocumentoController {
 
     @Autowired
@@ -59,6 +66,51 @@ public class InventarioDocumentoController {
         GenericResponse genericResponse = new GenericResponse();
         try {
             genericResponse = inventarioDocumentoServiceImpl.eliminarInventarioDocumento(inventarioDocumentoDto);
+            return genericResponse;
+        } catch (Exception e) {
+            genericResponse.setCodigoMensaje("500");
+            genericResponse.setMensaje("Ha ocurrido un error interno en el servidor: " + e.getMessage());
+            return genericResponse;
+        }
+    }
+
+    @PostMapping("/obtenerLista")
+    public GenericResponse listarInventarioDocumento(@Valid @RequestBody InventarioDocumentoDto inventarioDocumentoDto) {
+        GenericResponse genericResponse = new GenericResponse();
+        try {
+            List<InventarioDocumento> inventarios = inventarioDocumentoRepository.findByInventarioDocumentoExistentes(inventarioDocumentoDto.getPeriodo().getNroPeriodo());
+            List<Empleado> empleados = inventarioDocumentoRepository.findByInventarioDocumentoFaltantes(inventarioDocumentoDto.getPeriodo().getNroPeriodo());
+
+            // Verificamos si la lista está vacía
+            if (inventarios.isEmpty() && empleados.isEmpty()) {
+                genericResponse.setCodigoMensaje("404");
+                genericResponse.setMensaje("No existen datos para inventario de salarios");
+                return genericResponse;
+            }
+            
+            // Creamos un contenedor para la respuesta
+            List<Map<String, Object>> responseList = new ArrayList<>();
+
+            for (InventarioDocumento inventarioDocumento : inventarios) {
+                Map<String, Object> response = new HashMap<>();
+                response.put("inventarioDocumento", inventarioDocumento); 
+                responseList.add(response);
+            }
+
+            for (Empleado empleado : empleados) {
+                Map<String, Object> response = new HashMap<>();
+
+                InventarioDocumento inventarioDocumento = new InventarioDocumento();
+                inventarioDocumento.setEmpleado(empleado);
+
+                response.put("inventarioDocumento", inventarioDocumento); 
+                responseList.add(response);
+            }
+
+            genericResponse.setCodigoMensaje("200");
+            genericResponse.setMensaje("Han sido obtenidos los documentos de inventario salarial");
+            genericResponse.setObjeto(responseList);
+    
             return genericResponse;
         } catch (Exception e) {
             genericResponse.setCodigoMensaje("500");
--- src/main/java/com/gestionatalento/gestiona_talento/Repository/InventarioDocumentoRepository.java
+++ src/main/java/com/gestionatalento/gestiona_talento/Repository/InventarioDocumentoRepository.java
@@ -1,12 +1,15 @@
 package com.gestionatalento.gestiona_talento.Repository;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.gestionatalento.gestiona_talento.Entity.Empleado;
 import com.gestionatalento.gestiona_talento.Entity.InventarioDocumento;
 
 @Repository
@@ -18,4 +21,10 @@ public interface InventarioDocumentoRepository extends JpaRepository<InventarioD
     @Modifying
     @Query("DELETE FROM InventarioDocumento e WHERE e.id.nroPeriodo = :nroPeriodo AND e.id.codEmpleado = :codEmpleado")
     int deleteByInventarioDocumento(@Param("nroPeriodo") Long nroPeriodo, @Param("codEmpleado") Long codEmpleado);
+
+    @Query("SELECT d FROM InventarioDocumento d WHERE d.id.nroPeriodo = :nroPeriodo")
+    List<InventarioDocumento> findByInventarioDocumentoExistentes(@Param("nroPeriodo") Long nroPeriodo);
+
+    @Query("SELECT e FROM Empleado e WHERE NOT EXISTS(SELECT null FROM InventarioDocumento x WHERE x.empleado.codEmpleado = e.codEmpleado and x.id.nroPeriodo = :nroPeriodo)")
+    List<Empleado> findByInventarioDocumentoFaltantes(@Param("nroPeriodo") Long nroPeriodo);
 }
